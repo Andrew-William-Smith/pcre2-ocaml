70064a0df6a9a1863552f6843d5e6534d345eea4
Andrew-William-Smith <aws@awsmith.us>
2024-09-28 02:21:42 +0100

    Add tests for split

--- test/pcre2_tests.ml
+++ test/pcre2_tests.ml
@@ -1,15 +1,57 @@
 open OUnit2
 open Pcre2
 
-let test_full_split ctxt =
-  assert_equal 0 0
-  ; assert_equal [Text "ab"; Delim "x"; Group (1, "x"); NoGroup; Text "cd"]
+let test_split _ =
+  let assert_splits ?(max = -1) ~pat subj expected =
+    let actual = split ~max ~pat subj in
+    assert_equal (List.length expected) (List.length actual);
+    List.iter2 assert_equal expected actual
+  in
+  (* An empty string should split to nothing. *)
+  assert_splits ~pat:"" "" [];
+  assert_splits ~pat:"," "" [];
+  (* Split a string on a single character, allowing an unlimited number of
+     substrings to be returned with no truncation. *)
+  assert_splits ~pat:"," "a" ["a"];
+  assert_splits ~pat:"," "a," ["a"; ""];
+  assert_splits ~pat:"," "a,b" ["a"; "b"];
+  assert_splits ~pat:"," "a,,b" ["a"; ""; "b"];
+  assert_splits ~pat:"," "a,,b,," ["a"; ""; "b"; ""; ""];
+  (* If ~max:0, then trailing empty strings should be stripped. *)
+  assert_splits ~max:0 ~pat:"," "" [];
+  assert_splits ~max:0 ~pat:"," "," [];
+  assert_splits ~max:0 ~pat:"," "a,,b" ["a"; ""; "b"];
+  assert_splits ~max:0 ~pat:"," "a,,b,," ["a"; ""; "b"];
+  (* If a positive ~max match count is specified, then we should stop splitting
+     the string once that count has been reached. Trailing empty strings are not
+     stripped in this mode. *)
+  assert_splits ~max:1 ~pat:"," "a,b,c" ["a,b,c"];
+  assert_splits ~max:2 ~pat:"," "a,b,c" ["a"; "b,c"];
+  assert_splits ~max:3 ~pat:"," "a,b,c" ["a"; "b"; "c"];
+  assert_splits ~max:4 ~pat:"," "a,b,c" ["a"; "b"; "c"];
+  assert_splits ~max:4 ~pat:"," "a,b,c,," ["a"; "b"; "c"; ","];
+  assert_splits ~max:5 ~pat:"," "a,b,c,," ["a"; "b"; "c"; ""; ""];
+  (* Multi-character splits. *)
+  assert_splits ~pat:"<>" "" [];
+  assert_splits ~pat:"<>" "a<" ["a<"];
+  assert_splits ~pat:"<>" "a<>b<>c" ["a"; "b"; "c"];
+  assert_splits ~pat:"<>" "a<><>b<><>c" ["a"; ""; "b"; ""; "c"];
+  assert_splits ~max:0 ~pat:"<>" "a<><>b<><>c<><>" ["a"; ""; "b"; ""; "c"];
+  (* If capture groups are present in the split pattern, we should include them
+     in the output. *)
+  assert_splits ~pat:"([*+])" "" [];
+  assert_splits ~pat:"([*+])" "*" [""; "*"; ""];
+  assert_splits ~pat:"([*+])" "+" [""; "+"; ""];
+  assert_splits ~pat:"([*+])" "m*x+b" ["m"; "*"; "x"; "+"; "b"]
+
+let test_full_split _ =
+  assert_equal [Text "ab"; Delim "x"; Group (1, "x"); NoGroup; Text "cd"]
       (full_split ~pat:"(x)|(u)" "abxcd")
   ; assert_equal [Text "ab"; Delim "x"; Group (1, "x"); NoGroup; Text "cd"; Delim "u";
                   NoGroup; Group (2, "u"); Text "ef"]
       (full_split ~pat:"(x)|(u)" "abxcduef")
 
-let test_exec_all ctxt =
+let test_exec_all _ =
   let assert_matches ~pat subj expected =
     let rec assert_match_equal idx expected actual =
       match expected with
@@ -52,12 +94,9 @@ let test_exec_all ctxt =
     [[("", 0, 0); ("hello", 0, 5)]; [("", 4, 4); ("ocaml", 4, 9)]]
 
 let suite = "Test pcre2" >::: [
+      "test_split"      >:: test_split;
       "test_full_split" >:: test_full_split;
       "test_exec_all"   >:: test_exec_all;
     ]
 
-let _ = 
-if not !Sys.interactive then
-  run_test_tt_main suite
-else ()
-
+let _ = if not !Sys.interactive then run_test_tt_main suite
